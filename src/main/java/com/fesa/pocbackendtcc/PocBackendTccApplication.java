f25e92d72ee12ab3cd2d400d078ba78e953b069b
Guilherme Tavares <62484044+guiTavares13@users.noreply.github.com>
2023-04-29 11:43:50 -0300

    Create project

---
+++ src/main/java/com/fesa/pocbackendtcc/PocBackendTccApplication.java
@@ -0,0 +1,13 @@
+package com.fesa.pocbackendtcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBackendTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBackendTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fesa/pocbackendtcc/PocBackendTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fesa.pocbackendtcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBackendTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
